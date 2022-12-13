749393bfc02c1fe5681aaaa5c00c331dcf26af35
Princehakeem <princehakeem107@gmail.com>
2022-12-12 16:54:24 -0800

    101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * main - Print combinations of two digit numbers
+ * main - prints all possible different combinations of three digits.
  * Return: Always 0 (Success)
  */
 
